c3b5c3f3132c75b12c9cc01c890d365632f5e7ae
joudyhagar <joudykhaledhagar@gmail.com>
2025-11-12 15:14:28 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,18 @@
 #ifndef VISITOR_H
 #define VISITOR_H
 
-#include <string>
 using namespace std;
 
-//define visitor class here
+class Visitor 
+{
+    private:
+        string visitorName;
+        int ticketsBought;
 
+    public :
+        Visitor(string vn , int tb);
+        void displayInfo();
 
+};
 
-
-
-
-
-#endif
+#endif // VISITOR_H
